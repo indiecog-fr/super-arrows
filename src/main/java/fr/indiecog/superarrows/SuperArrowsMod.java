bb379abd86f3f98b9ac9a6dd394ec9748706b9c0
baptiste <baptiste.asselin@orange.fr>
2022-02-09 13:49:56 +0100

    initial commit in branch 1.18.1/dev

---
+++ src/main/java/fr/indiecog/superarrows/SuperArrowsMod.java
@@ -0,0 +1,10 @@
+package fr.indiecog.superarrows;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SuperArrowsMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        
+    }
+}
