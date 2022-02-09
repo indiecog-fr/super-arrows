3b377e47732e84f07303e4881491dc99c4d40b6c
baptiste <baptiste.asselin@orange.fr>
2022-02-09 18:05:43 +0100

    add class for glowing arrow item (no implementation yet)

---
+++ src/main/java/fr/indiecog/superarrows/item/arrow/GlowingArrowEntity.java
@@ -0,0 +1,9 @@
+package fr.indiecog.superarrows.item.arrow;
+
+import net.minecraft.item.ArrowItem;
+
+public class GlowingArrowEntity extends ArrowItem {
+    public GlowingArrowEntity(Settings settings) {
+        super(settings);
+    }
+}
