c0c354218748c4a1e64fea332d5b778d73a05639
baptiste <baptiste.asselin@orange.fr>
2022-02-10 23:43:53 +0100

    added texture + crafting recipe for glowing arrow and added lang file for translation of both glowing arrow and explosive arrow: ticket #1 #2

--- src/main/java/fr/indiecog/superarrows/entity/projectile/arrow/GlowingArrowEntity.java
+++ src/main/java/fr/indiecog/superarrows/entity/projectile/arrow/GlowingArrowEntity.java
@@ -1,58 +1,61 @@
 package fr.indiecog.superarrows.entity.projectile.arrow;
 
-import fr.indiecog.superarrows.SuperArrowsMod;
-import fr.indiecog.superarrows.item.arrow.GlowingArrowItem;
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.block.*;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.network.packet.s2c.play.EntityS2CPacket;
-import net.minecraft.state.property.Properties;
-import net.minecraft.util.BlockRotation;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
-import java.util.logging.Logger;
-
 public class GlowingArrowEntity extends ArrowEntity {
+
+    private boolean powerUsed;
+
     public GlowingArrowEntity(EntityType<? extends GlowingArrowEntity> entityType, World world) {
         super(entityType, world);
+        powerUsed = false;
     }
 
     public GlowingArrowEntity(World world, double x, double y, double z) {
         super(world, x, y, z);
+        powerUsed = false;
     }
 
     public GlowingArrowEntity(World world, LivingEntity owner) {
         super(world, owner);
+        powerUsed = false;
     }
 
 
     @Override
     protected void onBlockHit(BlockHitResult blockHitResult) {
         BlockState bs;
-        if(blockHitResult.getSide() == Direction.DOWN || !world.isAir(blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
-            dropItemtorch(blockHitResult.getBlockPos().offset(Direction.DOWN, 2));
-            return;
-        } else if(blockHitResult.getSide() == Direction.UP) {
-            bs = Blocks.TORCH.getDefaultState();
-        } else {
-            bs = Blocks.WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, blockHitResult.getSide());
+
+        if(!powerUsed) {
+            if(blockHitResult.getSide() == Direction.DOWN || !world.isAir(blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
+                dropItemtorch(blockHitResult.getBlockPos().offset(Direction.DOWN, 2));
+                powerUsed = true;
+                return;
+            } else if(blockHitResult.getSide() == Direction.UP) {
+                bs = Blocks.TORCH.getDefaultState();
+            } else {
+                bs = Blocks.WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, blockHitResult.getSide());
+            }
+
+            if(bs.canPlaceAt(world, blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
+                world.setBlockState(blockHitResult.getBlockPos().offset(blockHitResult.getSide()), bs, Block.NOTIFY_ALL);
+            } else {
+                dropItemtorch(blockHitResult.getBlockPos().offset(blockHitResult.getSide()));
+            }
+            powerUsed = true;
         }
 
-        if(bs.canPlaceAt(world, blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
-            world.setBlockState(blockHitResult.getBlockPos().offset(blockHitResult.getSide()), bs, Block.NOTIFY_ALL);
-        } else {
-            dropItemtorch(blockHitResult.getBlockPos().offset(blockHitResult.getSide()));
-        }
+
     }
 
     private void dropItemtorch(BlockPos pos) {
