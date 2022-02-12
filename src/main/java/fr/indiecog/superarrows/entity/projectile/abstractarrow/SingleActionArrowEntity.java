5d8c6a0bfe8dcb51e75fa7307d14a0e8e56a03a6
baptiste <baptiste.asselin@orange.fr>
2022-02-12 17:34:38 +0100

    added abstract class SingleActionArrowEntity representing an arrow entity with callback action triggered only once on first block or entity hit: ticket #5

---
+++ src/main/java/fr/indiecog/superarrows/entity/projectile/abstractarrow/SingleActionArrowEntity.java
@@ -0,0 +1,61 @@
+package fr.indiecog.superarrows.entity.projectile.abstractarrow;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+
+public abstract class SingleActionArrowEntity extends ArrowEntity {
+
+    private boolean isActionFinish;
+
+    public SingleActionArrowEntity(EntityType<? extends ArrowEntity> entityType, World world) {
+        super(entityType, world);
+        isActionFinish = false;
+    }
+
+    public SingleActionArrowEntity(World world, double x, double y, double z) {
+        super(world, x, y, z);
+        isActionFinish = false;
+    }
+
+    public SingleActionArrowEntity(World world, LivingEntity owner) {
+        super(world, owner);
+        isActionFinish = false;
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        super.onBlockHit(blockHitResult);
+        if(!isActionFinish) {
+            blockHitAction(blockHitResult);
+            isActionFinish = true;
+        }
+    }
+
+    @Override
+    protected void onHit(LivingEntity target) {
+        super.onHit(target);
+        if(!isActionFinish) {
+            hitAction(target);
+            isActionFinish = true;
+        }
+    }
+
+    @Override
+    protected void onEntityHit(EntityHitResult entityHitResult) {
+        super.onEntityHit(entityHitResult);
+        if(!isActionFinish) {
+            entityHitAction(entityHitResult);
+            isActionFinish = true;
+        }
+    }
+
+    protected abstract void blockHitAction(BlockHitResult blockHitResult);
+
+    protected abstract void hitAction(LivingEntity target);
+
+    protected abstract void entityHitAction(EntityHitResult entityHitResult);
+}
--- src/main/java/fr/indiecog/superarrows/entity/projectile/arrow/GlowingArrowEntity.java
+++ src/main/java/fr/indiecog/superarrows/entity/projectile/arrow/GlowingArrowEntity.java
@@ -1,79 +1,64 @@
 package fr.indiecog.superarrows.entity.projectile.arrow;
 
-import net.minecraft.block.*;
+import fr.indiecog.superarrows.entity.projectile.abstractarrow.SingleActionArrowEntity;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.WallTorchBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.ArrowEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
-public class GlowingArrowEntity extends ArrowEntity {
-
-    private boolean powerUsed;
+public class GlowingArrowEntity extends SingleActionArrowEntity {
 
     public GlowingArrowEntity(EntityType<? extends GlowingArrowEntity> entityType, World world) {
         super(entityType, world);
-        powerUsed = false;
     }
 
     public GlowingArrowEntity(World world, double x, double y, double z) {
         super(world, x, y, z);
-        powerUsed = false;
     }
 
     public GlowingArrowEntity(World world, LivingEntity owner) {
         super(world, owner);
-        powerUsed = false;
     }
-
 
     @Override
-    protected void onBlockHit(BlockHitResult blockHitResult) {
+    protected void blockHitAction(BlockHitResult blockHitResult) {
         BlockState bs;
 
-        if(!powerUsed) {
-            if(blockHitResult.getSide() == Direction.DOWN || !world.isAir(blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
-                dropItemtorch(blockHitResult.getBlockPos().offset(Direction.DOWN, 2));
-                powerUsed = true;
-                return;
-            } else if(blockHitResult.getSide() == Direction.UP) {
-                bs = Blocks.TORCH.getDefaultState();
-            } else {
-                bs = Blocks.WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, blockHitResult.getSide());
-            }
-
-            if(bs.canPlaceAt(world, blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
-                world.setBlockState(blockHitResult.getBlockPos().offset(blockHitResult.getSide()), bs, Block.NOTIFY_ALL);
-            } else {
-                dropItemtorch(blockHitResult.getBlockPos().offset(blockHitResult.getSide()));
-            }
-            powerUsed = true;
+        if(blockHitResult.getSide() == Direction.DOWN || !world.isAir(blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
+            dropItemtorch(blockHitResult.getBlockPos().offset(Direction.DOWN, 2));
+            return;
+        } else if(blockHitResult.getSide() == Direction.UP) {
+            bs = Blocks.TORCH.getDefaultState();
+        } else {
+            bs = Blocks.WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, blockHitResult.getSide());
         }
 
-
+        if(bs.canPlaceAt(world, blockHitResult.getBlockPos().offset(blockHitResult.getSide()))) {
+            world.setBlockState(blockHitResult.getBlockPos().offset(blockHitResult.getSide()), bs, Block.NOTIFY_ALL);
+        } else {
+            dropItemtorch(blockHitResult.getBlockPos().offset(blockHitResult.getSide()));
+        }
     }
+
+    @Override
+    protected void hitAction(LivingEntity target) {}
+
+    @Override
+    protected void entityHitAction(EntityHitResult entityHitResult) {}
 
     private void dropItemtorch(BlockPos pos) {
         // Create an item entity with velocity 0
         ItemEntity itemEntity = new ItemEntity(world, pos.north().getX(), pos.up().getY(), pos.getZ(), Items.TORCH.getDefaultStack(), 0, 0, 0);
         // Spawn the item entity
         world.spawnEntity(itemEntity);
-    }
-
-
-    @Override
-    public void initFromStack(ItemStack stack) {
-        super.initFromStack(stack);
-    }
-
-    @Override
-    public void onRemoved() {
-        super.onRemoved();
-        LOGGER.info("removed");
     }
 }
