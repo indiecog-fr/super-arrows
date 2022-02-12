e639e065cee6b11f43d59c82728ec16dbca2e388
TheDestiny45 <vgorgeon@gmail.com>
2022-02-12 19:36:25 +0100

    reworked item registering class

--- src/main/java/fr/indiecog/superarrows/item/SuperArrowsItems.java
+++ src/main/java/fr/indiecog/superarrows/item/SuperArrowsItems.java
@@ -12,18 +12,15 @@ import static fr.indiecog.superarrows.SuperArrowsMod.MODID;
 
 public class SuperArrowsItems {
 
-    public static final ExplosiveArrowItem EXPLOSIVE_ARROW_ITEM = register("explosive_arrow",new ExplosiveArrowItem(new FabricItemSettings().group(ItemGroup.COMBAT)));
-    public static final GlowingArrowItem GLOWING_ARROW_ITEM = register("glowing_arrow",new GlowingArrowItem(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item EXPLOSIVE_ARROW_ITEM = new ExplosiveArrowItem(new FabricItemSettings().group(ItemGroup.COMBAT));
+    public static final Item GLOWING_ARROW_ITEM = new GlowingArrowItem(new FabricItemSettings().group(ItemGroup.COMBAT));
 
-
-
-    private static <T extends Item> T register(String id, T item) {
+    private static void register(String id, Item item) {
         Registry.register(Registry.ITEM,new Identifier(MODID,id),item);
-        return item;
     }
 
     public static void init() {
-        //register("explosive_arrow",EXPLOSIVE_ARROW_ITEM);
-        //register("glowing_arrow",GLOWING_ARROW_ITEM);
+        register("explosive_arrow",EXPLOSIVE_ARROW_ITEM);
+        register("glowing_arrow",GLOWING_ARROW_ITEM);
     }
 }
